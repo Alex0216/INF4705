7d13d09025f65f83aa59141e01324accbaa2782f
Alexandre St-Onge <stonge.alex@gmail.com>
2015-02-10 16:04:06 -0500

    added mergesort

--- TP1/counting.cpp
+++ TP1/counting.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-#include <unordered_map>
 #include <vector>
 #include <algorithm>
 
--- TP1/mergeSeuil.cpp
+++ TP1/mergeSeuil.cpp
@@ -1,12 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <algorithm>
 
-void mergeSort();
+void mergeSort(std::vector<int> &array);
+void mergeSortRecursif(std::vector<int>& array, int low, int high);
+void getArray(std::string filename, std::vector<int> &array, int size);
 
-int main()
+int main(int argc, char* argv[])
 {
+
+    std::string file = argv[1];
+    int size = atoi(argv[2]);
+
+    std::vector<int> array;
+    array.reserve(size);
+    getArray(file, array, size);
+
+    std::clock_t    start;
+
+    start = std::clock();
+    mergeSort(array);
+
+    std::cout << std::endl;
+
+    std::cout <<  (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << std::endl;
+
     return 0;
 }
 
-void mergeSort()
+void mergeSort(std::vector<int> &array)
 {
+    mergeSortRecursif(array, 0, array.size());
+}
 
+void mergeSortRecursif(std::vector<int>& array, int low, int high) {
+    if (low == high)
+        return;
+
+    int middle = low + (high - low) / 2;
+
+    mergeSortRecursif(array, low, middle);
+    mergeSortRecursif(array, middle, high);
+
+    std::inplace_merge(std::begin(array)+low, std::begin(array)+middle, std::begin(array)+high);
+}
+
+void getArray(std::string filename, std::vector<int> &array, int size)
+{
+    std::fstream file(filename);
+    if(file.is_open())
+    {
+        int x;
+        for(int i = 0; i < size; ++i)
+        {
+            file >> x;
+            array.push_back(x);
+        }
+    }
 }
