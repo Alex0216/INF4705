51e1b7d74b1a32e9599b89b9115f025f8943b03d
Alexandre St-Onge <alexandre.st-onge@polymtl.ca>
2015-02-21 12:25:38 -0500

    modification de selection sort pour utiliser des vector comme les autres algo

--- TP1/selection.cpp
+++ TP1/selection.cpp
@@ -7,57 +7,48 @@ void getArray(std::string file, int* array, int size);
 
 int main(int argc, char* argv[])
 {
-
     std::string file = argv[1];
     int size = atoi(argv[2]);
 
-    int* array = new int[size];
-    getArray(file, array,size);
+    std::vector<int> array;
+    array.reserve(size);
+    getArray(file, array, size);
 
     std::clock_t    start;
 
     start = std::clock();
 
-    selectionSort(array, size);
+    selectionSort(array);
 
     std::cout <<  (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << std::endl;
 
     return 0;
 }
 
-void selectionSort(int* array, int size)
+void selectionSort(std::vector<int> &array)
 {
-    int min;
-
-    for(int i = 0; i < size; ++i)
+    Iterator min;
+	auto begin = array.begin();
+	auto end = array.end();
+    while (begin != end)
     {
-        min = i;
-        for(int j = i+1; j < size; ++j)
-        {
-            if(array[j] < array[min])
-            {
-                min = j;
-            }
-        }
-
-        if(min != i)
-        {
-            int temp = array[min];
-            array[min] = array[i];
-            array[i] = temp;
-        }
+        min = std::min_element(begin, end);
+        std::iter_swap(begin, min);
+        ++begin;
     }
 
 }
 
-void getArray(std::string filename, int* array, int size)
+void getArray(std::string filename, std::vector<int> &array, int size)
 {
     std::fstream file(filename);
     if(file.is_open())
     {
+        int x;
         for(int i = 0; i < size; ++i)
         {
-            file >> array[i];
+            file >> x;
+            array.push_back(x);
         }
     }
 }
