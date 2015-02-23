7967b2589a26eaafff5ec65bf3638f3c5bc31d1e
Alexandre St-Onge <stonge.alex@gmail.com>
2015-02-23 10:30:00 -0500

    mergeseuil complete

--- TP1/mergeSeuil.cpp
+++ TP1/mergeSeuil.cpp
@@ -3,10 +3,13 @@
 #include <ctime>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <chrono>
 
-void mergeSort(std::vector<int> &array);
-void mergeSortRecursif(std::vector<int>& array, int low, int high);
+void mergeSort(std::vector<int> &array, int seuil);
+void mergeSortRecursif(std::vector<int>& array, int low, int high, int seuil);
 void getArray(std::string filename, std::vector<int> &array, int size);
+void selectionSort(std::vector<int>::iterator begin, std::vector<int>::iterator end);
 
 int main(int argc, char* argv[])
 {
@@ -18,33 +21,47 @@ int main(int argc, char* argv[])
     array.reserve(size);
     getArray(file, array, size);
 
-    std::clock_t    start;
+    std::clock_t start;
+
 
     start = std::clock();
-    mergeSort(array);
+    mergeSort(array, 29);
 
+    std::cout <<  (std::clock() - start) / (double) (CLOCKS_PER_SEC / 1000) << ";";
     std::cout << std::endl;
-
-    std::cout <<  (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << std::endl;
-
     return 0;
 }
 
-void mergeSort(std::vector<int> &array)
+void mergeSort(std::vector<int> &array, int seuil)
 {
-    mergeSortRecursif(array, 0, array.size());
+    mergeSortRecursif(array, 0, array.size(), seuil);
 }
 
-void mergeSortRecursif(std::vector<int>& array, int low, int high) {
-    if (low == high)
+void mergeSortRecursif(std::vector<int>& array, int low, int high, int seuil) {
+    if (high - low <= seuil)
+    {
+        selectionSort(std::begin(array)+low, std::begin(array)+high);
         return;
+    }
 
     int middle = low + (high - low) / 2;
 
-    mergeSortRecursif(array, low, middle);
-    mergeSortRecursif(array, middle, high);
+    mergeSortRecursif(array, low, middle, seuil);
+    mergeSortRecursif(array, middle, high, seuil);
 
     std::inplace_merge(std::begin(array)+low, std::begin(array)+middle, std::begin(array)+high);
+}
+
+void selectionSort(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+    std::vector<int>::iterator min;
+    while (begin != end)
+    {
+        min = std::min_element(begin, end);
+        std::iter_swap(begin, min);
+        ++begin;
+    }
+
 }
 
 void getArray(std::string filename, std::vector<int> &array, int size)
--- TP1/selection.sh
+++ TP1/selection.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set +C
-echo "Selection Sort"
+#echo "Selection Sort"
 #Selection sort
-for i in 1000 5000 10000 50000 100000 500000
-do
-for file in ./tp1-H10-donnees/testset_${i}_{0..29}.txt;
-do
-    read -ra values <<< $(./Build/Selection $file $i)
-    echo $file";"$values
-done
-done
+#for i in 1000 5000 10000 50000 100000 500000
+#do
+#for file in ./tp1-H10-donnees/testset_${i}_{0..29}.txt;
+#do
+#    read -ra values <<< $(./Build/Selection $file $i)
+#    echo $file";"$values
+#done
+#done
 
 
 #Couting sort
